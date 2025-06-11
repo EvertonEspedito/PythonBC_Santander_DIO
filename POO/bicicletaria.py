5129dbe6c9a6e91f2dc8d501834d04eed6f74bcb
Everton Espedito <evertondev505@gmail.com>
2025-06-10 21:39:59 -0300

    Introduçao a POO
    Introdução básica a POO com Python!

---
+++ POO/bicicletaria.py
@@ -0,0 +1,22 @@
+class Bicicleta:
+    def __init__(self, valor, modelo, cor, ano):
+        self.cor = cor
+        self.valor =valor
+        self.modelo = modelo
+        self.ano = ano
+
+    def buzinar(self):
+        print("Bim Bim Bim")
+
+    def parar(self):
+        print("Parando...")
+        print("Parado!")
+
+    def correr(self):
+        print("Correndo...")
+
+b1 = Bicicleta(1500, "caloi 45", "Verde", 2040)    
+
+b1.buzinar()
+b1.correr()
+b1.parar()
