89d0d25df4d6269fa40ea786431479505bd1ed73
Everton Espedito <evertondev505@gmail.com>
2025-06-30 12:06:27 -0300

    Create dataHota.py

---
+++ pacotes/dataHota.py
@@ -0,0 +1,16 @@
+from datetime import date, time, timedelta, datetime
+
+data = date.today()
+print(f'Data de hoje: {data}')
+
+#adicionar 1 semana
+data = data + timedelta(days=7)
+print(f'Data daqui a 1 semana: {data}')
+
+#Horas
+hora = time(10, 30, 0)
+print(hora)
+
+# Data e hora
+dataAtual = datetime.now()
+print(f'Data e hora atual: {dataAtual}')
