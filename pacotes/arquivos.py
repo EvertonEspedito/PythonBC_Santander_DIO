a6fa30f9f7c662afdd96aec38a591994afce0dfe
Everton Espedito <evertondev505@gmail.com>
2025-07-02 11:33:59 -0300

    Update arquivos.py
    Leitura de arquivos

--- pacotes/arquivos.py
+++ pacotes/arquivos.py
@@ -9,3 +9,8 @@ print( file.readline()) #   lê a segunda linha do arquivo
 print( file.readline()) #  lê a terceira linha do arquivo
 file.close() # fecha o arquivo
 
+#Ler arquivo com while
+file = open( "arquivo.txt", "r" ) 
+while len (file.readline()) > 0: # enquanto houver linhas no arquivo
+    print(file.readline()) # imprime a linha
+file.close() # fecha o arquivo
