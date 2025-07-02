2c96a679e89e37114a08ffc0b566491d52747149
Everton Espedito <evertondev505@gmail.com>
2025-07-02 11:32:05 -0300

    Update arquivos.py

--- pacotes/arquivos.py
+++ pacotes/arquivos.py
@@ -2,3 +2,10 @@ file = open( "arquivo.txt", "r" ) # Abre o arquivo em modo leitura
 conteudo = file.read() # lê o conteúdo do arquivo
 file.close() # fecha o arquivo
 print(conteudo) # imprime o conteúdo do arquivo
+
+file = open( "arquivo.txt", "r" ) # Abre o arquivo em modo leitura
+print( file.readline()) #  lê a primeira linha do arquivo
+print( file.readline()) #   lê a segunda linha do arquivo
+print( file.readline()) #  lê a terceira linha do arquivo
+file.close() # fecha o arquivo
+
