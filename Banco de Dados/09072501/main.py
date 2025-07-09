ff4c7543b3706bb1ca28a1c2490f530499aab5e0
Everton Espedito <evertondev505@gmail.com>
2025-07-09 11:14:31 -0300

    Banco de Dados
    Criar, Inserir e Atualizar Dados com Python

--- Banco de Dados/09072501/main.py
+++ Banco de Dados/09072501/main.py
@@ -6,11 +6,22 @@ ROOT_PATH = Path(__file__).parent
 
 conexao = sqlite3.connect(ROOT_PATH/ "meu_banco.db")
 cursor = conexao.cursor()
-# criar tabelas
-cursor.execute('CREATE TABLE clientes (id INTEGER  PRIMARY KEY AUTOINCREMENT, nome VARCHAR(100), email VARCHAR(150))')
-# inserir registros
 
-data = ('Everton', 'everton@gmail.com')
-cursor.execute('INSERT INTO clientes (nome,email) VALUES(?,?)', data)
-conexao.commit()
+def criar_tabela(conexao, cursor,):
+    # criar tabelas
+    cursor.execute('CREATE TABLE clientes (id INTEGER  PRIMARY KEY AUTOINCREMENT, nome VARCHAR(100), email VARCHAR(150))')
+    conexao.commit()
 
+def inserir_registro(conexao, cursor,nome,email):
+    # inserir registros
+    data = (nome, email)
+    cursor.execute('INSERT INTO clientes (nome,email) VALUES(?,?)', data)
+    conexao.commit()
+
+def atualizar_registros(conexao, cursor,nome,email, id):   
+    #Atualização de Registros
+    data = (nome, email, id)
+    cursor.execute('UPDATE clientes SET nome = ?,email = ? WHERE id = ?',data) #Atualizar item/ WHERE é importante para atualizar apenas a linha desejada
+    conexao.commit()
+
+atualizar_registros(conexao, cursor, 'Everton Gostoso', 'Evertolindo@gmail.com', 1)
