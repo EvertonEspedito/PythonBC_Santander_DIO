d434335748352a0347031a4483c72cf5e5357186
Everton Espedito <evertondev505@gmail.com>
2025-07-12 00:10:06 -0300

    Buscas

---
+++ Banco de Dados/banco de dados/busca.py
@@ -0,0 +1,24 @@
+import sqlite3
+from pathlib import Path
+
+ROOT_PATH = Path(__file__).parent
+
+conexao = sqlite3.connect(ROOT_PATH/ "meu_banco.db")
+cursor = conexao.cursor()
+
+def buscar_id( cursor, id):
+    cursor.execute('SELECT * FROM clientes WHERE id = ?',(id,))#Retorna toda a linha
+    cursor.execute('SELECT nome FROM clientes WHERE id = ?',(id,))#Retorna apenas nome
+    resultado = cursor.fetchone()
+    return resultado
+
+def listar_clientes(cursor):
+    return cursor.execute('SELECT * FROM clientes')#Todos os dados de todas as linhas
+
+
+cliente = buscar_id(cursor,6)
+print(cliente)
+
+cliente = listar_clientes(cursor)
+for linha in cliente:
+    print(linha)
